618c89bc7ada5b8f05e4085523fd15fcc032ca14
Iman Randhawa <imandeep@gmail.com>
2024-03-20 12:55:29 -0500

    Update storage account.

--- terraform/azure/storage_account.tf
+++ terraform/azure/storage_account.tf
@@ -1,9 +1,10 @@
 resource "azurerm_storage_account" "example_storage_account" {
-  name                     = "examplestorageaccount"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  location                 = azurerm_resource_group.example_resource_group_for_storage.location
-  resource_group_name      = azurerm_resource_group.example_resource_group_for_storage.name
+  name                          = "examplestorageaccount"
+  account_tier                  = "Standard"
+  account_replication_type      = "GRS"
+  location                      = azurerm_resource_group.example_resource_group_for_storage.location
+  resource_group_name           = azurerm_resource_group.example_resource_group_for_storage.name
+  public_network_access_enabled = true
 }
 
 resource "azurerm_resource_group" "example_resource_group_for_storage" {
