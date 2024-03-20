9c4291fbc1f0b1532459f46f83c00920ddc4b936
Iman Randhawa <imandeep@gmail.com>
2024-03-20 12:55:29 -0500

    Update storage account.

--- terraform/azure/storage_account.tf
+++ terraform/azure/storage_account.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "example_storage_account" {
   resource_group_name      = azurerm_resource_group.example_resource_group_for_storage.name
 }
 
+
 resource "azurerm_resource_group" "example_resource_group_for_storage" {
   name     = "example-resource-group-for-storage"
   location = "West US 2"
